f334d9a9177624cbf462b9ba05dc27a0a8d8dc19
natalia-sa <natalia.andre@ccc.ufcg.edu.br>
2020-09-07 20:52:04 -0300

    criando o componente workstation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorkstationComponent } from './workstation/workstation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkstationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/workstation/workstation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workstation',
+  templateUrl: './workstation.component.html',
+  styleUrls: ['./workstation.component.css']
+})
+export class WorkstationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
